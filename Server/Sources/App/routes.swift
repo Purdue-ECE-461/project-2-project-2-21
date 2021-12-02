241e8da2b9a7bd1eb3d11252bbb8e3bc4f1b97ab
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2021-12-02 17:23:46 -0500

    Resolve name error

--- Server/Sources/App/routes.swift
+++ Server/Sources/App/routes.swift
@@ -1,7 +1,7 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    try app.register(collection: PackageIDController())
+    try app.register(collection: PackageController())
     try app.register(collection: AuthController())
     try app.register(collection: ResetController())
 }
