ae34662b04a5c52668081177bd6670c6baaea157
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2021-12-02 18:06:15 -0500

    Add history items

--- Server/Sources/App/Controllers/PackageController.swift
+++ Server/Sources/App/Controllers/PackageController.swift
@@ -12,10 +12,15 @@ struct PackageController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let package = routes.grouped("package")
         package.post(use: create) // Create or ingest a package
-        package.get(":id", use: index) // Get a package
-        package.put(":id", use: update) // Update a package
-        package.delete(":id", use: delete) // Deleta a package
-        package.get(":id", "rate", use: rate) // Rate a package
+        package.get(":id", use: index) // Get a package by ID
+        package.put(":id", use: update) // Update a package by ID
+        package.delete(":id", use: delete) // Deleta a package by ID
+        package.get(":id", "rate", use: rate) // Rate a package by ID
+        
+        package.group("byName") { route in
+            route.get(":name", use: getPackageByName) // Package By Name Get
+            route.delete(":name", use: deletePackageByName) // Delete all versions of this package.
+        }
     }
     
     func create(req: Request) throws -> ProjectPackage.Metadata {
@@ -49,6 +54,14 @@ struct PackageController: RouteCollection {
     func rate(request: Request) throws -> PackageScore {
         // TODO: Implement
         return PackageScore.mock
+    }
+    
+    func getPackageByName(request: Request) throws -> String {
+        return ""
+    }
+    
+    func deletePackageByName(request: Request) throws -> String {
+        return ""
     }
     
 }
