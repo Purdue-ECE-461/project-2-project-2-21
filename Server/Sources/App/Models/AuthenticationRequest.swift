cb55adb51d9a03feed058cfaf4f572d3790bc4b4
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2021-12-03 17:24:15 -0500

    Remove DEBUG flags (#55)
    * Random change to trigger gcloud
    
    * Remove DEBUG flags

--- Server/Sources/App/Models/AuthenticationRequest.swift
+++ Server/Sources/App/Models/AuthenticationRequest.swift
@@ -29,7 +29,7 @@ extension AuthenticationRequest {
     }
 }
 
-#if DEBUG
+// TODO: Remove this
 extension AuthenticationRequest {
     static let mock = AuthenticationRequest(
         user: User(
@@ -41,4 +41,3 @@ extension AuthenticationRequest {
         )
     )
 }
-#endif
