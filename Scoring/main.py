58b84614b0ff4fdb875366ba4e2014b38027dc38
Jason Lei <lei56@purdue.edu>
2021-12-04 17:55:54 -0500

    Created github actions yaml for automatic deployment to google cloud functions

---
+++ Scoring/main.py
@@ -0,0 +1,19 @@
+"""This mamin module is the google cloud functions wrapper for perform.py"""
+
+import json
+
+def score_firestore_change(data, context):
+    """Triggered by a change to a Firestore document
+    args:
+        data (dict): the event payload
+        context (google.cloud.functions.Context): Metadata for the event.
+    """
+    trigger_resource = context.resource
+    
+    print(function triggered by change to %s' % trigger_resource)
+    
+    print('\n01d value:')
+    print(json.dumps(data["oldValue"]))
+    
+    print('\nNew value:')
+    print(json.dumps(data["value"]))
