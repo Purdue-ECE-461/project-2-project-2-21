3ec29e0c795ac3aae088e0566f726c19572097dd
reid63 <reid63@purdue.edu>
2021-12-04 20:00:36 -0500

    Added Initial Deletion

--- frontend/frontsite/urls.py
+++ frontend/frontsite/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('upload/',views.upload,name='upload'),
     path('download/',views.download,name='download'),
     path('packages/',views.PackageListView.as_view(),name='package_list'),
     path('packages/upload/',views.UploadPackageView.as_view(),name='upload_package'),
+    path('packages/<pk>',views.DeletePackageView.as_view(),name='delete_package')
 ]
 
 if settings.DEBUG:
--- frontend/frontsite/views.py
+++ frontend/frontsite/views.py
@@ -1,41 +1,12 @@
 from django.shortcuts import redirect, render, redirect
 from django.core.files.storage import FileSystemStorage
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DeleteView
 from .forms import PackageForm
 from .models import Package
 
 # Create your views here.
 def index(request):
     return render(request,'frontsite/index.html',{})
-
-#inital upload test
-def upload(request):
-    context = {}
-    if request.method == 'POST':
-        uploaded_file = request.FILES['document']
-        fs = FileSystemStorage()
-        name = fs.save(uploaded_file.name, uploaded_file)
-        context['url'] = fs.url(name)
-    return render(request,'frontsite/upload.html',context)
-
-#package upload
-# def package_list(request):
-#     packages = Package.objects.all()
-#     return render(request, 'frontsite/package_list.html', {
-#         'packages': packages
-#     })
-
-# def upload_package(request):
-#     if request.method == 'POST':
-#         form = PackageForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('package_list')
-#     else:
-#         form = PackageForm()
-#     return render(request, 'frontsite/upload_package.html', {
-#         'form': form
-#     })
 
 #package upload class based
 class PackageListView(ListView):
@@ -48,6 +19,11 @@ class UploadPackageView(CreateView):
     fields = ('title','uppackage')
     success_url = '/packages/'
     template_name = 'frontsite/upload_package.html'
+
+#package deletion
+class DeletePackageView(DeleteView):
+    model = Package
+    success_url = '/packages/'
 
 def download(request):
     return render(request,'frontsite/download.html',{})
