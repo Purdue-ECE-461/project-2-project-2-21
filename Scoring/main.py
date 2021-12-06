5a9deef3ddfcc890c0b475661ab04027c1985533
lei56 <lei56@purdue.edu>
2021-12-06 15:24:22 -0500

    Added update event trigger function workflow. Implemented simple firebase doc change parsing

--- Scoring/main.py
+++ Scoring/main.py
@@ -2,6 +2,9 @@
 
 import json
 from localpackage import perform
+from google.cloud import firestore
+client = firesstore.Client()
+
 
 def score_firestore_change(data, context):
     """Triggered by a change to a Firestore document
@@ -19,5 +22,17 @@ def score_firestore_change(data, context):
     print('\nNew value:')
     print(json.dumps(data["value"]))
     
-    perform.perform_single("https://github.com/expressjs/express")
-    print("success!!")
+    path_parts = context.resource.split('/documents/'[1].split('/')
+    collection_path = path_parts[0]
+    document_path = '/'.join(path_parts[1:]
+    
+    affected_doc = client.collection(collection_path).document(document_path)
+    
+    content_value = data["value"]["fields"]["content"]
+    url_value = data["value"]["fields"]["url"]
+    print(content_value)
+    print(url_value)
+    
+    
+    #perform.perform_single("https://github.com/expressjs/express")
+    #print("success!!")
