91703e2bbe30ad2d45821834ee46c66fbe58ea87
lei56 <lei56@purdue.edu>
2021-12-06 20:25:58 -0500

    Baseline implementation to upload scores to the database

--- Scoring/main.py
+++ Scoring/main.py
@@ -12,16 +12,33 @@ def score_firestore_change(data, context):
         data (dict): the event payload
         context (google.cloud.functions.Context): Metadata for the event. 
     """   
+    # Retrieve changed/new document
     path_parts = context.resource.split('/documents/packages/')[1].split('/')
     collection_path = path_parts[0]
     document_path = '/'.join(path_parts[1:])
     
     affected_doc = client.collection(collection_path).document(document_path)
     
-    content_value = data["value"]["fields"]["content"]
-    url_value = data["value"]["fields"]["url"]
-    print(content_value)
+    # Retrieve appropriate fields
+    id_value = data["value"]["fields"]["ID"]["stringValue"]
+    url_value = data["value"]["fields"]["URL"]["stringValue"]
+    version_value = data["value"]["fields"]["Version"]["stringValue"]
+    print(id_value)
     print(url_value)
     
-    perform.perform_single(url_value)
+    # Score based on url
+    [net_score, ramp_up_score, correctness_score, bus_factor, 
+        responsiveness_score, license_score, update_score] = perform.perform_single(url_value)
     print("success!!")
+    
+    # Write scores to new file
+    new_doc = db.collection(u'scores').document(id_value)
+    new_doc.set({
+        u'URL' : url_value,
+        u'ID'  : id_value,
+        u'Version': version_value
+    })
+    
+    
+
+    
