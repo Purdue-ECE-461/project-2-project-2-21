d24032492f944b54e6d36481bc65cab49730e4b4
lei56 <lei56@purdue.edu>
2021-12-08 00:57:57 -0500

    Set the document contents to be a string instead of blob

--- ingest/localpackage/testing.py
+++ ingest/localpackage/testing.py
@@ -1,3 +1,3 @@
 import ingest
 
-ingest.ingest_package_link("https://github.com/jashkenas/underscore", "1.0.0")
+ingest.ingest_package_link("https://github.com/expressjs/express", "4.17.1")
--- ingest/main.py
+++ ingest/main.py
@@ -26,5 +26,5 @@ def ingest_package(data, context):
 
         # Write ingested zip to affected document
         affected_doc.set({
-            'content': zipf
+            'content': unicode(zipf)
         }, merge=True)
