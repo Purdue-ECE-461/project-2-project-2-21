75fb2035da5c57ca477160b41cd20b898bbdefe5
reid63 <reid63@purdue.edu>
2021-12-08 18:07:22 -0500

    HTML ADA Compliance

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-=n7wma#@9thi62$+4!-)b@9r2iv7d7w%akh1lj_i2bb1r@33-v'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = 'django-insecure-=n7wma#@9thi62$+4!-)b@9r2iv7d7w%akh1lj_i2bb1r@33-v'
+#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
